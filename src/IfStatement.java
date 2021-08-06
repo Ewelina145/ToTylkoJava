5473c84612b4467b34b3975b7c64b189ea654d8f
Ewelina Świerzbińska <ewelina.swierzbinska01@gmail.com>
2021-08-06 18:22:20 +0200

    Lesson 1

---
+++ src/IfStatement.java
@@ -0,0 +1,11 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        int humanHeight=180;
+
+        if(humanHeight >=180){
+            System.out.println("Człowiek jest wysoki");
+        }else{
+            System.out.println( );
+        }
+    }
+}
